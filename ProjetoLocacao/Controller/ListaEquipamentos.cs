81193329352032462cbaa5f919a7b73c926f9ad8
Italo Carvalho <italo.998.carvalho@gmail.com>
2018-06-26 21:42:57 -0300

    Criada a pasta Controller e a classe ListaEquipamentos

---
+++ ProjetoLocacao/Controller/ListaEquipamentos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoLocacao.Controller
+{
+    class ListaEquipamentos
+    {
+        public int salvar { get; set; }
+    }
+}
